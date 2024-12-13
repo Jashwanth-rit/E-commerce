79379f6d78391c46b54beb8620b2d11594d9f23e
Jashwanth-rit <158207370+Jashwanth-rit@users.noreply.github.com>
2024-12-13 23:00:09 +0530

    added inmemory database

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
 const jsonServer = require("json-server");
-const cors = require("cors"); // Importing CORS middleware
+const cors = require("cors");
 const server = jsonServer.create();
-const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
+const router = jsonServer.router({}); // Empty in-memory object instead of db.json
 const port = process.env.PORT || 8080;
 
-// Custom CORS configuration
 const corsOptions = {
-  origin: `${process.env.FRONTEND_URL}`, // Replace with your front-end URL
+  origin: process.env.FRONTEND_URL || "http://localhost:3000",
   methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"],
 };
 
-server.use(cors(corsOptions)); // Apply CORS middleware
+server.use(cors(corsOptions));
 server.use(middlewares);
 server.use(router);
 
